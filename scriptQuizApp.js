ef0757a6530464dbf93a396028fa5d8d7207e8df
Rohit Jaiswar <65119113+Rohitjaiswar123@users.noreply.github.com>
2023-06-07 15:30:32 +0530

    Add files via upload

---
+++ scriptQuizApp.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: "which year is going on right now?",
+        a: '2000',
+        b: '2015',
+        c: '2030',
+        d: '2022',
+        correct: 'd'
+    },
+    {
+        question: "how old is rohit?",
+        a: '20',
+        b: '25',
+        c: '88',
+        d: '22',
+        correct: "d"
+    },
+    {
+        question: "what does HTML stand for?",
+        a: "hyperText markup language",
+        b: "HYperText many Language",
+        c: "Hightext mew Lang",
+        d:"hellTo moron lang",
+        correct: "a"
+    },
+{
+    question: "Who is the president of US?",
+    a: "Donald duck",
+    b: "Gold D roger",
+    c: "Whitebeard",
+    d: "Barrak obama",
+    correct:"a"
+},
+{
+    question:"what is the most used programming language?",
+    a:"c++",
+    b:"c",
+    c:"javascript",
+    d:'Node.js',
+    correct: "c"
+}
+]
